4a48c8699943cee14ecd6021c6cb3acab9abf491
franciscoluis86 <franciscoluis86@gmail.com>
2024-07-01 17:09:20 -0400

    publish branch

---
+++ First-class.py
@@ -0,0 +1,11 @@
+print("Hola Nicaragua")
+if 5 > 2:
+    print("Five is greater than two!")
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Alice"))
+
+def howmuchmoneydoihave(money):
+    return f"Hello, you have CAD {money} in your account"
+print(howmuchmoneydoihave(1))
